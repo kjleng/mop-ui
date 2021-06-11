e8560e0c8d5259c2342839b76c838712d4080809
Patrick McLennan <Patrick.McLennan@fairstone.ca>
2021-06-11 15:18:06 -0400

    Merchant details form + accordion base

--- src/components/AddMerchantModal/AddMerchantModal.tsx
+++ src/components/AddMerchantModal/AddMerchantModal.tsx
@@ -1,4 +1,21 @@
-import { makeStyles, Theme, Typography, Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, FormLabel, Select } from "@material-ui/core";
+import { 
+    makeStyles, 
+    Theme, 
+    Typography, 
+    Button, 
+    Dialog, 
+    DialogTitle, 
+    DialogContent, 
+    DialogContentText, 
+    TextField, 
+    DialogActions, 
+    FormLabel, 
+    Select, 
+    Grid, 
+    Accordion, 
+    AccordionSummary 
+} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { useTranslation } from 'react-i18next';
 import React from 'react'
 import CloseIcon from '@material-ui/icons/Close';
@@ -54,15 +71,91 @@ const useStyles = makeStyles((theme) => ({
         gridColumnStart: 2,
         justifySelf: 'right',
         alignSelf: 'center',
+    },
+    merchantFoundForm: {
+        display: `flex`,
+        padding: `24px`,
+        flexDirection: `column`,
+        [theme.breakpoints.up(`md`)]: {
+            display: `grid`,
+            gridGap: `24px`,
+            gridTemplateAreas: `
+                'merchantName nothing'
+                'displayName nothing'
+                'merchantId ecomId'
+                'drawer drawer'
+            `
+        },
+        '& .merchantName': {
+            gridArea: `merchantName`,
+        },
+        '& .displayName': {
+            gridArea: `displayName`,
+        },
+        '& .merchantId': {
+            gridArea: `merchantId`,
+        },
+        '& .ecomId': {
+            gridArea: `ecomId`,
+        },
+        '& input': {
+            fontSize: `24px`,
+            lineHeight: `30px`,
+        },
+        '& label.Mui-focused': {
+            color: `#3f2a56`
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: `#3f2a56`
+        }
+    },
+    merchantFoundInput: {
+        fontSize: `24px`,
+        lineHeight: `30px`,
+        color: `#333`,
+    },
+    merchantFoundLabel: {
+        color: `#333`,
+        fontSize: `14px`,
+        lineHeight: `normal`,
+        fontWeight: 600
+    },
+    merchantFoundDrawer: {
+        gridArea: `drawer`,
+        border: `none`,
+        '&::before': {
+            display: `none`
+        },
+        '& .summary': {
+            flexDirection: `row-reverse`,
+            paddingLeft: 0
+        },
+        '& .MuiAccordionSummary-expandIcon': {
+            padding: `0`,
+            marginRight: `12px`,
+            color: `#333`
+        },
+        '& .header': {
+            fontSize: `16px`,
+            lineHeight: `20px`,
+            fontWeight: 600,
+            color: `#333`
+        }
     }
 }));
 
 export const AddMerchantModal: React.FC<AddMerchantModalProps> = ({ isOpen }) => {
     const classes = useStyles();
-
     
     const [open, setOpen] = React.useState(isOpen); //whether Modal is open
     const [submitText, setSubmitText] = React.useState('Search Merchant');
+
+    const [merchantFoundForm, setMerchantFoundForm] = React.useState({
+        displayName: ``,
+        merchantId: ``,
+        ecomId: ``,
+        additionalDetailsExpanded: false
+    })
 
     const handleClose = () => {
         // different api calls depending on submitText var's value
@@ -90,8 +183,6 @@ export const AddMerchantModal: React.FC<AddMerchantModalProps> = ({ isOpen }) =>
                         name='serach-merchant-name'
                         label="Search Merchant Name"
                     />
-
-
                 </form>
 
                 {
@@ -99,7 +190,88 @@ export const AddMerchantModal: React.FC<AddMerchantModalProps> = ({ isOpen }) =>
                     // https://app.zeplin.io/project/60afaeb937ae977eae3bcbd1/screen/60b6869fe0a7bb3862848e3f
                     // https://app.zeplin.io/project/60afaeb937ae977eae3bcbd1/screen/60b6869f9c7b12757015693a
                 }
+                <form className={classes.merchantFoundForm} onSubmit={submitSearchMerchant}>
+                    <TextField
+                        className="merchantName"
+                        id="merchantName"
+                        InputProps={{
+                            className: classes.merchantFoundInput,
+                            disableUnderline: true,
+                            readOnly: true 
+                        }}
+                        InputLabelProps={{
+                            className: classes.merchantFoundLabel
+                        }}
+                        label="Merchant"
+                        defaultValue="Lorem Ipsum" // TODO: replace this with Merchant name from API response
+                    />
+                    <TextField
+                        className="displayName"
+                        id="displayName"
+                        InputProps={{ 
+                            className: classes.merchantFoundInput,
+                            value: merchantFoundForm.displayName, 
+                            onChange: ({ target: { value } }) => setMerchantFoundForm(prevForm => ({
+                                ...prevForm,
+                                displayName: value
+                            }))
+                        }}
+                        InputLabelProps={{
+                            className: classes.merchantFoundLabel
+                        }}
+                        label="Display Name"
+                    />
+                    <TextField
+                        className="merchantId"
+                        id="merchantId"
+                        InputProps={{ 
+                            className: classes.merchantFoundInput,
+                            value: merchantFoundForm.merchantId, 
+                            onChange: ({ target: { value } }) => setMerchantFoundForm(prevForm => ({
+                                ...prevForm,
+                                merchantId: value
+                            }))
+                        }}
+                        InputLabelProps={{
+                            className: classes.merchantFoundLabel
+                        }}
+                        label="Merchant Id"
+                    />
+                    <TextField
+                        className="ecomId"
+                        id="ecomId"
+                        InputProps={{ 
+                            className: classes.merchantFoundInput,
+                            value: merchantFoundForm.ecomId, 
+                            onChange: ({ target: { value } }) => setMerchantFoundForm(prevForm => ({
+                                ...prevForm,
+                                ecomId: value
+                            }))
+                        }}
+                        InputLabelProps={{
+                            className: classes.merchantFoundLabel
+                        }}
+                        label="EComm Store ID"
+                    />
+                    <Accordion 
+                        className={classes.merchantFoundDrawer}
+                        elevation={0}
+                        expanded={merchantFoundForm.additionalDetailsExpanded} 
+                        onChange={() => setMerchantFoundForm(prevForm => ({
+                            ...prevForm,
+                            additionalDetailsExpanded: !prevForm.additionalDetailsExpanded
+                        }))}
+                    >
+                        <AccordionSummary className="summary" expandIcon={<ExpandMoreIcon style={{ padding: 0 }} fontSize="large" />}>
+                            <Typography className="header">Additional Details</Typography>
+                        </AccordionSummary>
 
+                        {/**
+                         * TODO:
+                         *   Accordion options will go here
+                         */}
+                    </Accordion>
+                </form>
 
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
